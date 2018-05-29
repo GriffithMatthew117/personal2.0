2ef03ab5cc07a8ba0888a7930da1927a8e489d66
griffithmatthew117@gmail.com <matthew.griffith117@gmail.com>
2018-05-28 21:39:44 -0700

    fixing server file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import { normalize } from 'path';
+
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -9,23 +11,42 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
 
-// set the view engine to ejs
+//Here we're setting the views directory to be ./views
+//thereby letting the app know where to find the template files
+app.set('views', './views');
+
+
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public/ionicons'))
+//Here we're setting the default engine to be ejs
+//note we don't need to require it, express will do all that for us
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page 
-app.get('/', function(req, res) {
-    res.render('pages/index');
+//Now instead of using res.send we can use
+//res.render to send the output of the template by filename
+app.get('/index', (req, res) => {
+  res.render('index');
 });
 
+//var port = normalizePort(process.env.PORT || '8080');
+var portInfo = process.env.PORT || '8080';
+var port = parseInt(portInfo, 10);
 
-app.post('/index', (req, res) => {
 
-  })
+app.listen(port, () => {
+  console.log('listening at port ' + port)
+});
 
-app.listen(8080);
-console.log('Listening on that 8080 bump');
+app.get('/', (req, res) => {
+  const data = {
+    person: {
+      firstName: req.body.name
+    }
+  }
+
+  // Notice now the data is the second argument passed to the template render method
+  res.render('index', data);
+});
