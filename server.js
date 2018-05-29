7a9bb9eb9081d6f642ee24d5fe280805b6629ce8
griffithmatthew117@gmail.com <matthew.griffith117@gmail.com>
2018-05-28 21:22:07 -0700

    Added a Procfile.

--- server.js
+++ server.js
@@ -5,21 +5,6 @@ var twilio = require('twilio');
 
 const app = express();
 
-
-var accountSid = 'AC211492b5174e07ed6c58644c74b65aa6'; // Your Account SID from www.twilio.com/console
-var authToken = '73dedda1ab9d02e27118cd6d6f86b13f'; // Your Auth Token from www.twilio.com/console
-
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
-
-client.messages.create({
-    body: 'Hello from Node',
-    to: '+15634598306', // Text this number
-    from: '+17608915959' // From a valid Twilio number
-  })
-.then((message) => console.log(message.sid));
-
-
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -39,32 +24,7 @@ app.get('/', function(req, res) {
 
 
 app.post('/index', (req, res) => {
-    var accountSid = 'AC211492b5174e07ed6c58644c74b65aa6'; // Your Account SID from www.twilio.com/console
-    var authToken = '73dedda1ab9d02e27118cd6d6f86b13f'; // Your Auth Token from www.twilio.com/console
-  
-    var twilio = require('twilio');
-    var client = new twilio(accountSid, authToken);
-  
-  
-    const data = {
-      person: {
-        firstName: req.body.firstName
-      }
-    }
-  
-    client.messages.create({
-        name: req.body.firstName,
-        email: req.body.email,
-        object: req.body.subject,
-        body: `${req.body.firstName}  just contacted you: Email: ${req.body.email} Subject: ${req.body.subject} Message: ${req.body.message}`,
-        to: '+15634598306',
-        from: '+17608915959'
-      })
-      .then((message) => {
-        console.log(message.sid);
-        console.log("Data is ", data);
-        res.render('contact', data);
-      });
+
   })
 
 app.listen(8080);
